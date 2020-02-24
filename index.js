236b2fea74aa7023bce55f39b25e7ef976349306
Ariunbold <ariun1118@gmail.com>
2020-02-23 19:15:50 -0500

    Project Route is finished

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require("express");
+const actionRouter = require("./actionRouter")
+const projectRouter = require("./projectRouter")
 
-Sing along:
+const server = express();
+const port = 4000
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.use(express.json());
 
-Go code!
-*/
+server.use("/api/actions", actionRouter)
+server.use("/api/projects", projectRouter)
+
+server.use((req, res) => {
+	res.status(404).json({
+		message: "Route was not found",
+	})
+})
+
+server.use((err, req, res, next) => {
+    console.log(err)
+      res.status(500).json({
+        message: 'Error retrieving the projects'
+   })
+  })
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
